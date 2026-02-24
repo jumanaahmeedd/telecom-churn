56edeb6b3b1e8f4be44677a8e8a6f8a6864c7070
Jumana Ahmed <jumanaazima@gmail.com>
2026-02-24 22:58:44 +0200

    add requirementstxt

--- api/app.py
+++ api/app.py
@@ -1,4 +1,60 @@
-import pandas
+import pandas as pd 
+import joblib
 from flask import Flask, request
-model = 
+model = joblib.load(r'C:\Users\juman\OneDrive\Desktop\Prog\telecom-churn\models\telcomdata.joblib')
+
 app = Flask(__name__)
+def prepare_data(data):
+    gender = data.get('gender')
+    SeniorCitizen = data.get('SeniorCitizen')
+    Partner = data.get('Partner')
+    Dependents = data.get('Dependents')
+    tenure = data.get('tenure')
+    PhoneService = data.get('PhoneService')
+    MultipleLines = data.get('MultipleLines')
+    InternetService = data.get('InternetService')
+    OnlineSecurity = data.get('OnlineSecurity')
+    OnlineBackup = data.get('OnlineBackup')
+    DeviceProtection = data.get('DeviceProtection')
+    TechSupport = data.get('TechSupport')
+    StreamingTV = data.get('StreamingTV')
+    StreamingMovies = data.get('StreamingMovies')
+    Contract = data.get('Contract')
+    PaperlessBilling = data.get('PaperlessBilling')
+    PaymentMethod = data.get('PaymentMethod')
+    MonthlyCharges = data.get('MonthlyCharges')
+    TotalCharges = data.get('TotalCharges')
+    example_customer = pd.DataFrame({
+        "gender": [gender],
+        "SeniorCitizen": [SeniorCitizen],
+        "Partner": [Partner],
+        "Dependents": [Dependents],
+        "tenure": [int(tenure)],
+        "PhoneService": [PhoneService],
+        "MultipleLines": [MultipleLines],
+        "InternetService": [InternetService],
+        "OnlineSecurity": [OnlineSecurity],
+        "OnlineBackup": [OnlineBackup],
+        "DeviceProtection": [DeviceProtection],
+        "TechSupport": [TechSupport],
+        "StreamingTV": [StreamingTV],
+        "StreamingMovies": [StreamingMovies],
+        "Contract": [Contract],
+        "PaperlessBilling": [PaperlessBilling],
+        "PaymentMethod": [PaymentMethod],
+        "MonthlyCharges": [float(MonthlyCharges)],
+        "TotalCharges": [float(TotalCharges)]
+    })
+    return example_customer
+
+@app.route('/predict', methods='GET')
+def predict():
+    data = request.data
+    prepared_data = prepare_data(data)
+    pred = model.predict(prepared_data)[0]
+    if pred == 'Yes':
+        return 'Churn'
+    else:
+        return 'Not Churn'
+    
+app.run()
